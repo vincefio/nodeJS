8df51a943ce84460939437539b2e23acc7018e00
Vince <vincentfiorilli@gmail.com>
2018-06-19 21:03:07 -0400

    firebase and form working

--- read.js
+++ read.js
@@ -1,5 +1,10 @@
 const fs = require('fs')
 
+fs.appendFile('./message.txt', 'test append', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 fs.readFile('./message.txt', 'utf-8', function (err, data) {
   if (err) throw err;
   console.log(data);
