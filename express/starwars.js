fde77e837705dfaae6273e78f86da89fbcad6176
Vince <vincentfiorilli@gmail.com>
2018-07-14 09:18:44 -0400

    more routing

--- express/starwars.js
+++ express/starwars.js
@@ -1,7 +1,10 @@
 const express = require('express')
 var app = express()
+var path = require('path')
 
-
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, "view.html"));
+})
 
 app.get('/api/:person/:age', function(req, res){
   var person = req.params.person
@@ -9,5 +12,10 @@ app.get('/api/:person/:age', function(req, res){
   res.send('person ' + person + ' age ' + age)
   //res.send('Hello World!')
 })
+
+//post request
+app.post("/api/new", function(req, res) {
+  res.send('get ready for post!')
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
