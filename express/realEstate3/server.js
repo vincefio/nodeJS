e8e1b57a4ff5cc3c1fe9a3a0bcd2a6b152d84cf6
Vince <vincentfiorilli@gmail.com>
2018-07-18 17:32:48 -0400

    photo rendering working

---
+++ express/realEstate3/server.js
@@ -0,0 +1,59 @@
+//dependencies
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var mysql = require("mysql")
+
+//set up multer
+var multer  = require('multer')
+
+//set storage engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+//init upload
+const upload = multer({
+  storage: storage
+}).single('houseView')
+
+//set up app
+var app = express()
+var PORT = 8080
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+//ejs
+app.set('view engine', 'ejs')
+
+//public folder
+app.use(express.static('./public'))
+
+//set up home route
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/sell', (req, res) => res.render('sell'))
+
+app.post('/postSell', (req, res) =>{
+  upload(req, res, (err) => {
+    //res.send('uploads/' + req.file.filename)
+    res.render('sell', {
+          msg: 'File Uploaded!',
+          file: `uploads/${req.file.filename}`
+        });
+  })
+})
+
+//start server
+app.listen(PORT, () => console.log('listening on port ' + PORT))
