dd38dd333fe0fe9ae515f5c8116701b0cccf983f
Vince <vincentfiorilli@gmail.com>
2018-07-18 06:26:58 -0400

    not much progress

--- express/realEstate2/server.js
+++ express/realEstate2/server.js
@@ -1,5 +1,4 @@
 //add dependencies
-//dependencies
 var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
@@ -8,6 +7,19 @@ const ejs = require('ejs')
 
 //set up multer
 var multer  = require('multer')
+
+//set storage engine
+const storage = multer.diskStorage({
+  destination: '.public/uploads/',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+//init upload
+const upload = multer({
+  storage: storage
+}).single('houseView')
 
 //set up app
 var app = express()
@@ -31,6 +43,31 @@ app.use(express.static('./public'))
 
 //set up home route
 app.get('/', (req, res) => res.render('index'))
+
+app.get('/sell', (req, res) => res.render('sell'))
+
+app.post('/postSell', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('sell', {
+        msg: err
+      })
+      console.log('ERROR')
+    }else{
+      //req.file is the info we would send to a database
+      if(req.file == undefined){
+        res.render('sell', {
+          msg: 'Error: No File Selected!'
+        });
+      } else{
+        res.render('sell', {
+          msg: 'File Uploaded!',
+          file: `uploads/${req.file.filename}`
+        });
+      }
+    }
+  })
+})
 
 //start server
 app.listen(PORT, () => console.log('listening on port ' + PORT))
