dde235ec445e38068efc98ef66ebef13f150ae74
Vince <vincentfiorilli@gmail.com>
2018-07-24 18:19:34 -0400

    doin stuff w tings

--- express/burger/config/orm.js
+++ express/burger/config/orm.js
@@ -11,7 +11,13 @@ var orm = {
     })
   },
   add: function(name, cb){
-    connection.query('INSERT INTO burgers(burger_name) VALUES (?)', ['testy belesty'], function(err, result){
+  /*  connection.query('INSERT INTO burgers(burger_name) VALUES (?)', [name], function(err, result){
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    })*/
+    connection.query('INSERT INTO burgers SET ?', {burger_name: name}, function(err, result){
       if (err) {
         throw err;
       }
