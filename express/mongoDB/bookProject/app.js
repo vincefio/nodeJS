77096e7c6610d3f602b4e3a61c79ac78ade84256
Vince <vincentfiorilli@gmail.com>
2018-07-28 10:38:09 -0400

    mongo connected

--- express/mongoDB/bookProject/app.js
+++ express/mongoDB/bookProject/app.js
@@ -10,6 +10,15 @@ var PORT = 8080
 //initialize bodyParser
 app.use(bodyParser.json())
 
+//connect to mongoose
+mongoose.connect('mongodb://localhost/bookStore');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('mongo connected')
+});
+
 //set up static folder
 app.use(express.static(__dirname + '/public'))
 
